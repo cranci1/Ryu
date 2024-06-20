b1545b416cd79f5c85bef7b35dc58f0f5d458cd6
cranci1 <trevisanfrancesco022@gmail.com>
2024-06-20 14:58:09 +0200

    Initial Commit

---
+++ AnimeLounge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeLounge
+//
+//  Created by Francesco on 20/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
