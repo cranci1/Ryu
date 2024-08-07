925f4bf52abebc01189f5ebad9de772e396c9d24
cranci1 <trevisanfrancesco022@gmail.com>
2024-08-07 10:57:55 +0200

    Revert "test for background downloads"
    This reverts commit 1862abbcee6e51abf0fac55616c7b70655893bb8.

--- AnimeLounge/Utils/Downloaders/MP4Downloader.swift
+++ AnimeLounge/Utils/Downloaders/MP4Downloader.swift
@@ -7,66 +7,61 @@
 
 import Foundation
 
-class MP4Downloader: NSObject, URLSessionDownloadDelegate {
-    private var progressHandler: ((Float) -> Void)?
-    private var completion: ((Result<URL, Error>) -> Void)?
-    
-    private lazy var backgroundSession: URLSession = {
-        let configuration = URLSessionConfiguration.background(withIdentifier: "me.cranci.animelounge.background")
-        configuration.waitsForConnectivity = true
-        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-    }()
-    
-    static let shared = MP4Downloader()
-    
-    private override init() {
-        super.init()
-    }
-    
-    func downloadFile(from urlString: String, progressHandler: @escaping (Float) -> Void, completion: @escaping (Result<URL, Error>) -> Void) {
-        self.progressHandler = progressHandler
-        self.completion = completion
-        
+class MP4Downloader {
+    static func downloadFile(from urlString: String, progressHandler: @escaping (Float) -> Void, completion: @escaping (Result<URL, Error>) -> Void) {
         guard let url = URL(string: urlString) else {
             completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
             return
         }
         
-        let request = URLRequest(url: url)
-        let task = backgroundSession.downloadTask(with: request)
+        let configuration = URLSessionConfiguration.default
+        configuration.waitsForConnectivity = true
+        
+        let session = URLSession(configuration: configuration)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.cachePolicy = .reloadIgnoringLocalCacheData
+        
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let tempLocalUrl = tempLocalUrl,
+                  let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError(domain: "Invalid response or no data received", code: 0, userInfo: nil)))
+                return
+            }
+            
+            let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let originalFileName = url.lastPathComponent
+            let destinationFileUrl = Self.getUniqueFileURL(for: originalFileName, in: documentsDirectoryURL)
+            
+            do {
+                try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                completion(.success(destinationFileUrl))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
         task.resume()
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        guard let httpResponse = downloadTask.response as? HTTPURLResponse,
-              (200...299).contains(httpResponse.statusCode) else {
-            let error = NSError(domain: "Invalid response or no data received", code: 0, userInfo: nil)
-            completion?(.failure(error))
-            return
+        
+        let progressObserver = task.progress.observe(\.fractionCompleted, options: [.new]) { progress, _ in
+            DispatchQueue.main.async {
+                progressHandler(Float(progress.fractionCompleted))
+            }
         }
         
-        let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let originalFileName = downloadTask.originalRequest?.url?.lastPathComponent ?? "episode.mp4"
-        let destinationFileUrl = Self.getUniqueFileURL(for: originalFileName, in: documentsDirectoryURL)
-        
-        do {
-            try FileManager.default.moveItem(at: location, to: destinationFileUrl)
-            completion?(.success(destinationFileUrl))
-        } catch {
-            completion?(.failure(error))
-        }
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
-        DispatchQueue.main.async {
-            self.progressHandler?(progress)
-        }
-    }
-    
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error {
-            completion?(.failure(error))
+        var stateObserver: NSKeyValueObservation?
+        stateObserver = task.observe(\.state, options: [.new]) { task, _ in
+            if task.state == .completed || task.state == .canceling {
+                progressObserver.invalidate()
+                stateObserver?.invalidate()
+            }
         }
     }
     
