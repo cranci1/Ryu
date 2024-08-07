1285902ea40be796d8bac7cd8864b1311cb51f86
cranci1 <trevisanfrancesco022@gmail.com>
2024-08-07 11:51:41 +0200

    source info settings

---
+++ AnimeLounge/Swift - Views/Settings/SubPages/SettingsViewDoneButton.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewDoneButton.swift
+//  AnimeLounge
+//
+//  Created by Francesco on 07/08/24.
+//
+
+import UIKit
+
+class SettingsViewDoneButton: UITableViewController {
+    
+    @IBAction func closeButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+}
