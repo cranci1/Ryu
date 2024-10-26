e40440b83b290ab5e97bef74924f56ff433220a3
Francesco <trevisanfrancesco022@gmail.com>
2024-10-26 17:26:19 +0200

    Kitsu login

--- Ryu/Swift - Views/Settings/SubPages/SettingsAnimeListing.swift
+++ Ryu/Swift - Views/Settings/SubPages/SettingsAnimeListing.swift
@@ -10,13 +10,17 @@ import Security
 
 class SettingsAnimeListing: UITableViewController {
     
-    @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var loginLogoutLabel: UILabel!
+    @IBOutlet weak var anilistStatusLabel: UILabel!
+    @IBOutlet weak var anilistLoginLogoutLabel: UILabel!
+    @IBOutlet weak var kitsuStatusLabel: UILabel!
+    @IBOutlet weak var kitsuLoginLogoutLabel: UILabel!
     
     @IBOutlet var pushUpdatesSwitch: UISwitch!
     
-    let serviceName = "me.ryu.AniListToken"
-    let accountName = "AniListAccessToken"
+    let anilistServiceName = "me.ryu.AniListToken"
+    let anilistAccountName = "AniListAccessToken"
+    let kitsuServiceName = "me.ryu.KitsuToken"
+    let kitsuAccountName = "KitsuAccessToken"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +30,16 @@ class SettingsAnimeListing: UITableViewController {
         updateUserStatus()
         loadUserDefaultss()
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(loginLogoutLabelTapped))
-        loginLogoutLabel.isUserInteractionEnabled = true
-        loginLogoutLabel.addGestureRecognizer(tapGesture)
+        let anilistTapGesture = UITapGestureRecognizer(target: self, action: #selector(loginLogoutLabelTapped))
+        anilistLoginLogoutLabel.isUserInteractionEnabled = true
+        anilistLoginLogoutLabel.addGestureRecognizer(anilistTapGesture)
+        
+        let kitsuTapGesture = UITapGestureRecognizer(target: self, action: #selector(kitsuLoginLogoutLabelTapped))
+        kitsuLoginLogoutLabel.isUserInteractionEnabled = true
+        kitsuLoginLogoutLabel.addGestureRecognizer(kitsuTapGesture)
         
         NotificationCenter.default.addObserver(self, selector: #selector(handleAuthorizationCode(_:)), name: Notification.Name("AuthorizationCodeReceived"), object: nil)
+        updateKitsuUserStatus()
     }
     
     func loadUserDefaultss() {
@@ -49,13 +58,13 @@ class SettingsAnimeListing: UITableViewController {
         if let _ = getTokenFromKeychain() {
             showLogoutConfirmation()
         } else {
-            statusLabel.text = "Starting authentication..."
+            anilistStatusLabel.text = "Starting authentication..."
             AniListLogin.authenticate()
         }
     }
     
     func showLogoutConfirmation() {
-        let alert = UIAlertController(title: "Log Out", message: "Are you sure you want to log out?", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Log Out", message: "Are you sure you want to log out from AniList?", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Log Out", style: .destructive) { _ in
@@ -67,7 +76,7 @@ class SettingsAnimeListing: UITableViewController {
     
     func logout() {
         removeTokenFromKeychain()
-        statusLabel.text = "You are not logged in"
+        anilistStatusLabel.text = "You are not logged in"
         updateLoginLogoutLabelText()
     }
     
@@ -81,10 +90,10 @@ class SettingsAnimeListing: UITableViewController {
         AniListToken.exchangeAuthorizationCodeForToken(code: code) { [weak self] success in
             DispatchQueue.main.async {
                 if success {
-                    self?.statusLabel.text = "Login successful! Updating user info..."
+                    self?.anilistStatusLabel.text = "Login successful! Updating user info..."
                     self?.updateUserStatus()
                 } else {
-                    self?.statusLabel.text = "Login failed. Please try again."
+                    self?.anilistStatusLabel.text = "Login failed. Please try again."
                 }
             }
         }
@@ -92,7 +101,7 @@ class SettingsAnimeListing: UITableViewController {
     
     func updateStatusForTokenExchange() {
         DispatchQueue.main.async {
-            self.statusLabel.text = "Exchanging code for token..."
+            self.anilistStatusLabel.text = "Exchanging code for token..."
         }
     }
     
@@ -101,7 +110,7 @@ class SettingsAnimeListing: UITableViewController {
             pushUpdatesSwitch.isEnabled = true
             fetchUserInfo(token: token)
         } else {
-            statusLabel.text = "You are not logged in"
+            anilistStatusLabel.text = "You are not logged in"
             pushUpdatesSwitch.isEnabled = false
         }
         updateLoginLogoutLabelText()
@@ -132,7 +141,7 @@ class SettingsAnimeListing: UITableViewController {
             request.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
         } catch {
             DispatchQueue.main.async {
-                self.statusLabel.text = "Failed to serialize JSON: \(error.localizedDescription)"
+                self.anilistStatusLabel.text = "Failed to serialize JSON: \(error.localizedDescription)"
             }
             return
         }
@@ -140,14 +149,14 @@ class SettingsAnimeListing: UITableViewController {
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 DispatchQueue.main.async {
-                    self.statusLabel.text = "Error: \(error.localizedDescription)"
+                    self.anilistStatusLabel.text = "Error: \(error.localizedDescription)"
                 }
                 return
             }
             
             guard let data = data else {
                 DispatchQueue.main.async {
-                    self.statusLabel.text = "No data received"
+                    self.anilistStatusLabel.text = "No data received"
                 }
                 return
             }
@@ -170,18 +179,18 @@ class SettingsAnimeListing: UITableViewController {
                         let usernameRange = (fullText as NSString).range(of: username)
                         attributedText.addAttribute(.foregroundColor, value: color, range: usernameRange)
                         
-                        self.statusLabel.attributedText = attributedText
+                        self.anilistStatusLabel.attributedText = attributedText
                         self.pushUpdatesSwitch.isEnabled = true
                         self.loadUserDefaultss()
                     }
                 } else {
                     DispatchQueue.main.async {
-                        self.statusLabel.text = "Unexpected response format"
+                        self.anilistStatusLabel.text = "Unexpected response format"
                     }
                 }
             } catch {
                 DispatchQueue.main.async {
-                    self.statusLabel.text = "Failed to parse JSON: \(error.localizedDescription)"
+                    self.anilistStatusLabel.text = "Failed to parse JSON: \(error.localizedDescription)"
                 }
             }
         }
@@ -213,14 +222,14 @@ class SettingsAnimeListing: UITableViewController {
     func updateLoginLogoutLabelText() {
         let isLoggedIn = getTokenFromKeychain() != nil
         let labelText = isLoggedIn ? "Log Out from AniList.co" : "Log In with AniList.co"
-        loginLogoutLabel.text = labelText
+        anilistLoginLogoutLabel.text = labelText
     }
     
     func getTokenFromKeychain() -> String? {
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: serviceName,
-            kSecAttrAccount as String: accountName,
+            kSecAttrService as String: anilistServiceName,
+            kSecAttrAccount as String: anilistAccountName,
             kSecReturnData as String: kCFBooleanTrue!,
             kSecMatchLimit as String: kSecMatchLimitOne
         ]
@@ -238,8 +247,155 @@ class SettingsAnimeListing: UITableViewController {
     func removeTokenFromKeychain() {
         let deleteQuery: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: serviceName,
-            kSecAttrAccount as String: accountName
+            kSecAttrService as String: anilistServiceName,
+            kSecAttrAccount as String: anilistAccountName
+        ]
+        SecItemDelete(deleteQuery as CFDictionary)
+    }
+    
+    @objc func kitsuLoginLogoutLabelTapped() {
+        if let _ = getKitsuTokenFromKeychain() {
+            showKitsuLogoutConfirmation()
+        } else {
+            showKitsuLoginPrompt()
+        }
+    }
+    
+    func showKitsuLoginPrompt() {
+        let alert = UIAlertController(title: "Login to Kitsu", message: "Enter your email/username and password", preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Email or Username"
+            textField.autocapitalizationType = .none
+            textField.autocorrectionType = .no
+        }
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Login", style: .default) { [weak self] _ in
+            guard let username = alert.textFields?[0].text,
+                  let password = alert.textFields?[1].text,
+                  !username.isEmpty, !password.isEmpty else {
+                      self?.kitsuStatusLabel.text = "Please enter both username and password"
+                      return
+                  }
+            
+            self?.kitsuStatusLabel.text = "Logging in..."
+            
+            KitsuToken.authenticateUser(username: username, password: password) { success in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.kitsuStatusLabel.text = "Login successful! Updating user info..."
+                        self?.updateKitsuUserStatus()
+                    } else {
+                        self?.kitsuStatusLabel.text = "Login failed. Please try again."
+                    }
+                }
+            }
+        })
+        
+        present(alert, animated: true)
+    }
+    
+    func showKitsuLogoutConfirmation() {
+        let alert = UIAlertController(title: "Log Out", message: "Are you sure you want to log out from Kitsu?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Log Out", style: .destructive) { [weak self] _ in
+            self?.kitsuLogout()
+        })
+        
+        present(alert, animated: true)
+    }
+    
+    func kitsuLogout() {
+        removeKitsuTokenFromKeychain()
+        kitsuStatusLabel.text = "You are not logged in"
+        updateKitsuLoginLogoutLabelText()
+    }
+    
+    func updateKitsuUserStatus() {
+        if let token = getKitsuTokenFromKeychain() {
+            fetchKitsuUserInfo(token: token)
+        } else {
+            kitsuStatusLabel.text = "You are not logged in"
+        }
+        updateKitsuLoginLogoutLabelText()
+    }
+    
+    func fetchKitsuUserInfo(token: String) {
+        let userInfoURL = URL(string: "https://kitsu.io/api/edge/users?filter[self]=true")!
+        var request = URLRequest(url: userInfoURL)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/vnd.api+json", forHTTPHeaderField: "Accept")
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self?.kitsuStatusLabel.text = "Error: \(error.localizedDescription)"
+                    return
+                }
+                
+                guard let data = data else {
+                    self?.kitsuStatusLabel.text = "No data received"
+                    return
+                }
+                
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                       let data = json["data"] as? [[String: Any]],
+                       let firstUser = data.first,
+                       let attributes = firstUser["attributes"] as? [String: Any],
+                       let username = attributes["name"] as? String {
+                        
+                        self?.kitsuStatusLabel.text = "Logged in as \(username)"
+                    } else {
+                        self?.kitsuStatusLabel.text = "Failed to parse user info"
+                    }
+                } catch {
+                    self?.kitsuStatusLabel.text = "Failed to parse response: \(error.localizedDescription)"
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func updateKitsuLoginLogoutLabelText() {
+        let isLoggedIn = getKitsuTokenFromKeychain() != nil
+        kitsuLoginLogoutLabel.text = isLoggedIn ? "Log Out from Kitsu" : "Log In with Kitsu"
+    }
+    
+    func getKitsuTokenFromKeychain() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: kitsuServiceName,
+            kSecAttrAccount as String: kitsuAccountName,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        
+        guard status == errSecSuccess,
+              let tokenData = item as? Data else {
+                  return nil
+              }
+        
+        return String(data: tokenData, encoding: .utf8)
+    }
+    
+    func removeKitsuTokenFromKeychain() {
+        let deleteQuery: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: kitsuServiceName,
+            kSecAttrAccount as String: kitsuAccountName
         ]
         SecItemDelete(deleteQuery as CFDictionary)
     }
